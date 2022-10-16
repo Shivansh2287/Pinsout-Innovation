9896f1c03b4ee460570d5090e5be9b9092f0c056
shivansh <shivansh211299@gmail.com>
2022-10-16 22:01:12 +0530

    related product

--- components/review.jsx
+++ components/review.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { AiTwotoneStar, AiOutlineStar } from "react-icons/ai";
 import { MdVerified } from "react-icons/md";
 import p1 from "../assets/p1.png";
@@ -12,36 +12,111 @@ import {
   Button,
   Divider,
 } from "@chakra-ui/react";
-import Image from 'next/image';
+import Image from "next/image";
+import { BsHandThumbsUp } from "react-icons/bs";
 
 export default function Review() {
   return (
-    <Stack marginTop="5%">
+    <Stack marginTop="5%" direction="column" px={16} py={10}>
       <Stack direction="row">
-        <Image
-          src={p1}
-          width={50}
-          height={50}
-          style={{
-            borderRadius: "50%",
-          }}
-        />
-        <Text>Viola Lucas</Text>
-        <MdVerified />
-        <Text>August 13, 2022</Text>
-        <Stack direction="row">
-          <AiTwotoneStar />
-          <AiTwotoneStar />
-          <AiTwotoneStar />
-          <AiTwotoneStar />
-          <AiOutlineStar />
+        <Stack width="50px">
+          <Image
+            src={p1}
+            width={50}
+            height={50}
+            style={{
+              borderRadius: "50%",
+            }}
+          />
         </Stack>
-        <Text>
-          Its really easy to use and it is exactly what I am looking for. A lot
-          of good looking templates & its highly customizable. Live support is
-          helpful, solved my issue in no time.
-        </Text>
+        <Stack direction="column">
+          <Stack direction="row">
+            <Text>Viola Lucas</Text>
+            <MdVerified />
+            <Stack>
+              <Button rightIcon={<BsHandThumbsUp />}>Was This helpful?</Button>
+            </Stack>
+          </Stack>
+          <Text>August 13, 2022</Text>
+          <Stack direction="row">
+            <AiTwotoneStar />
+            <AiTwotoneStar />
+            <AiTwotoneStar />
+            <AiTwotoneStar />
+            <AiOutlineStar />
+          </Stack>
+          <Text fontSize="22px">
+            Its really easy to use and it is exactly what I am looking for. A
+            lot of good looking templates & its highly customizable. Live
+            support is helpful, solved my issue in no time.
+          </Text>
+        </Stack>
       </Stack>
+      <Divider />
+      <Stack pt={5} direction="row">
+        <Stack width="50px">
+          <Image
+            src={p2}
+            width={50}
+            height={50}
+            style={{
+              borderRadius: "50%",
+            }}
+          />
+        </Stack>
+        <Stack direction="column">
+          <Stack direction="row">
+            <Text>Viola Lucas</Text>
+            <MdVerified />
+          </Stack>
+          <Text>August 13, 2022</Text>
+          <Stack direction="row">
+            <AiTwotoneStar />
+            <AiTwotoneStar />
+            <AiTwotoneStar />
+            <AiTwotoneStar />
+            <AiOutlineStar />
+          </Stack>
+          <Text fontSize="22px">
+            Its really easy to use and it is exactly what I am looking for. A
+            lot of good looking templates & its highly customizable. Live
+            support is helpful, solved my issue in no time.
+          </Text>
+        </Stack>
+      </Stack>
+      <Divider />
+      <Stack pt={5} direction="row">
+        <Stack width="50px">
+          <Image
+            src={p3}
+            width={50}
+            height={50}
+            style={{
+              borderRadius: "50%",
+            }}
+          />
+        </Stack>
+        <Stack direction="column">
+          <Stack direction="row">
+            <Text>Viola Lucas</Text>
+            <MdVerified />
+          </Stack>
+          <Text>August 13, 2022</Text>
+          <Stack direction="row">
+            <AiTwotoneStar />
+            <AiTwotoneStar />
+            <AiTwotoneStar />
+            <AiTwotoneStar />
+            <AiOutlineStar />
+          </Stack>
+          <Text fontSize="22px">
+            Its really easy to use and it is exactly what I am looking for. A
+            lot of good looking templates & its highly customizable. Live
+            support is helpful, solved my issue in no time.
+          </Text>
+        </Stack>
+      </Stack>
+      <Divider />
     </Stack>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import ProductSection from "../components/ProductSection";
 import RatingReview from "../components/RatingReview";
 import RelatedProducts from "../components/RelatedProducts";
+import RelateProd from "../components/RelateProd";
 import Review from "../components/review";
 import styles from "../styles/Home.module.css";
 
@@ -14,7 +16,9 @@ export default function Home() {
       <ProductSection />
       <RelatedProducts />
       <RatingReview />
-      <Review/>
+      <Review />
+      <RelateProd />
+      <Footer />
     </>
   );
 }
